04acadb0f1373254bd06d6792b9ba5ca7d575de6
camilamoldes <camilamoldes@gmail.com>
2026-05-17 11:50:28 +0200

    Update email link to include mailto subject

--- src/data/cv-data.ts
+++ src/data/cv-data.ts
@@ -13,7 +13,7 @@ export const profile: ProfileData = {
     links: [
         {
             label: "Email",
-            url: "camilamoldes@gmail.com",
+            url: "mailto:camilamoldes@gmail.com?subject=Contacto%20desde%20la%20web",
             icon: "email",
         }
     ]
